6f74d91a7ecc8ace7a69ace9c46b176a449f1f45
Rohith M <rohith171801@gmail.com>
2025-07-27 17:09:32 +0530

    feat: text_to_speech

--- Functions/text_to_speech.py
+++ Functions/text_to_speech.py
@@ -5,10 +5,12 @@ import os
 async def text_to_speech(text: str) -> bool:
     try:
         base_dir = os.path.dirname(__file__)
-        output_path = os.path.join(base_dir, "..", "output_audios", "audio.mp3")
+        output_path = os.path.join(base_dir, "..", "output_audios", "audio_out.mp3")
         if os.path.exists(output_path):
             os.remove(output_path)
-        communicate = edge_tts.Communicate(text, "en-US-AriaNeural", rate="+10%")
+        communicate = edge_tts.Communicate(
+            text, "en-US-ChristopherNeural", rate="+20%", pitch="-20Hz"
+        )
 
         await communicate.save(output_path)
         return True
